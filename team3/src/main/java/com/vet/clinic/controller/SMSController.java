27aeaa3c7a98c5285dbd7e425be3b4666fcba99d
miseon jeong <miseon jeong@DESKTOP-0CT5PIQ>
2023-03-20 14:36:48 +0900

    3/20 재고관리

--- team3/src/main/java/com/vet/clinic/controller/SMSController.java
+++ team3/src/main/java/com/vet/clinic/controller/SMSController.java
@@ -1,6 +1,5 @@
 package com.vet.clinic.controller;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -9,7 +8,6 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
---
+++ team3/src/main/java/com/vet/clinic/service/StockService.java
@@ -0,0 +1,31 @@
+package com.vet.clinic.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.vet.clinic.dao.StockDAO;
+import com.vet.clinic.dto.StockDTO;
+
+@Service
+public class StockService {
+	
+	@Autowired
+	public StockDAO stockDAO;
+
+	public List<StockDTO> stockList() {
+		return stockDAO.stockList();
+	}
+
+	public List<String> m_company() {
+		return stockDAO.m_company();
+	}
+
+	public List<String> m_subcate() {
+		return stockDAO.m_subcate();
+	}
+	
+	
+	
+}
